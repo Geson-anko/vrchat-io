84473f4c1bc65e9d89e5990fc61b8adbb6d8bdca
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-07-04 17:25:30 +0900

    ADD OSC address container class

---
+++ vrchat_io/osc/addresses.py
@@ -0,0 +1,42 @@
+"""This file contains OSC addresses for the VRChat.
+
+See official docs: https://docs.vrchat.com/docs/osc-overview OSC as Input Controller:
+"""
+
+# OSC addresses for input controller.
+# See official docs: https://docs.vrchat.com/docs/osc-as-input-controller
+
+
+class Axes:
+    Vertical = "/input/Vertical"
+    Horizontal = "/input/Horizontal"
+    LookHorizontal = "/input/LookHorizontal"
+    UseAxisRight = "/input/UseAxisRight"
+    GrabAxisRight = "/input/GrabAxisRight"
+    MoveHoldFB = "/input/MoveHoldFB"
+    SpinHoldCwCcw = "/input/SpinHoldCwCcw"
+    SpinHoldUD = "/input/SpinHoldUD"
+    SpinHoldLR = "/input/SpinHoldLR"
+
+
+class Buttons:
+    MoveForward = "/input/MoveForward"
+    MoveBackward = "/input/MoveBackward"
+    MoveLeft = "/input/MoveLeft"
+    MoveRight = "/input/MoveRight"
+    LookLeft = "/input/LookLeft"
+    LookRight = "/input/LookRight"
+    Jump = "/input/Jump"
+    Run = "/input/Run"
+    ComfortLeft = "/input/ComfortLeft"
+    ComfortRight = "/input/ComfortRight"
+    DropRight = "/input/DropRight"
+    UseRight = "/input/UseRight"
+    GrabRight = "/input/GrabRight"
+    DropLeft = "/input/DropLeft"
+    UseLeft = "/input/UseLeft"
+    GrabLeft = "/input/GrabLeft"
+    PanicButton = "/input/PanicButton"
+    QuickMenuToggleLeft = "/input/QuickMenuToggleLeft"
+    QuickMenuToggleRight = "/input/QuickMenuToggleRight"
+    Voice = "/input/Voice"
