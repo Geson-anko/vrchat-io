c6b6a1047badb8c680a5218d422553356bb77de8
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-07-25 17:35:24 +0900

    ADD VideoCaptureWrapper class

--- tests/abc/test_video_capture.py
+++ tests/abc/test_video_capture.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
+from pytest_mock import MockerFixture
 
-from vrchat_io.abc.video_capture import VideoCapture
+from vrchat_io.abc.video_capture import VideoCapture, VideoCaptureWrapper
 
 
 class VideoCaptureImpl(VideoCapture):
@@ -20,3 +21,25 @@ class TestVideoCapture:
     def test_error_of_abstract_class_instantiation(self):
         with pytest.raises(TypeError):
             VideoCapture()
+
+
+class VideoCaptureWrapperImpl(VideoCaptureWrapper):
+    def after_read(self, frame: np.ndarray) -> np.ndarray:
+        return frame
+
+
+class TestVideoCaptureWrapper:
+    def test_instantiation(self):
+        VideoCaptureWrapperImpl(VideoCaptureImpl())
+
+    def test_call_after_read(self, mocker: MockerFixture):
+        video_capture_wrapper = VideoCaptureWrapperImpl(VideoCaptureImpl())
+
+        meth = mocker.spy(video_capture_wrapper, "after_read")
+        frame = video_capture_wrapper.read()
+        meth.assert_called_once_with(frame)
+
+    def test_error_of_abstract_class_instantiation(self):
+        video_capture = VideoCaptureImpl()
+        with pytest.raises(TypeError):
+            VideoCaptureWrapper(video_capture)
--- vrchat_io/abc/video_capture.py
+++ vrchat_io/abc/video_capture.py
@@ -1,6 +1,8 @@
-"""This file contains the abstract video capture class."""
+"""This file contains the abstract video capture class and its wrapper
+class."""
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 import numpy as np
 
@@ -21,3 +23,41 @@ class VideoCapture(ABC):
             np.ndarray: The frame read from the video capture.
         """
         pass
+
+
+class VideoCaptureWrapper(VideoCapture, ABC):
+    """Wrapper class for video capture.
+
+    This class wraps the return value of :meth:`read` method of the video capture class.
+    Please implement the :meth:`after_read` method to process the frame.
+
+    You can access base video capture object by ``self._video_capture``.
+    """
+
+    def __init__(self, video_capture: VideoCapture, *args: Any, **kwds: Any) -> None:
+        """Initialize the video capture wrapper.
+
+        Args:
+            video_capture (VideoCapture): The video capture to be wrapped.
+        """
+        self._video_capture = video_capture
+
+    @abstractmethod
+    def after_read(self, frame: np.ndarray) -> np.ndarray:
+        """Process the frame read from the video capture.
+
+        Args:
+            frame (np.ndarray): The frame read from the video capture.
+
+        Returns:
+            np.ndarray: The processed frame.
+        """
+        pass
+
+    def read(self) -> np.ndarray:
+        """Read a frame from the video capture.
+
+        Returns:
+            np.ndarray: The processed frame read from the video capture.
+        """
+        return self.after_read(self._video_capture.read())
