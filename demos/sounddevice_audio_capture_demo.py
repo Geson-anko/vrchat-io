5634018d13e903c20ab82d79ed1367508b3c26bd
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-10-26 18:28:01 +0900

    Add demo script for SounddeviceAudioCapture

---
+++ demos/sounddevice_audio_capture_demo.py
@@ -0,0 +1,28 @@
+"""This demo shows how to use SounddeviceAudioCapture class."""
+from pathlib import Path
+
+import numpy as np
+import sounddevice as sd
+import soundfile as sf
+
+from vrchat_io.audio import SounddeviceAudioCapture
+
+devices = sd.query_devices()
+print("--- devices ---")
+print(devices)
+print()
+print("selecting: device", sd.default.device[0])
+print("---------------")
+
+capture = SounddeviceAudioCapture(samplerate=16000, device=sd.default.device[0], frame_size=1600, channels=1)  # 0.1 sec
+
+
+frames = []
+for i in range(10):  # 1 sec
+    frames.append(capture.read())
+    print("Frame count:", i + 1)
+wave_data = np.concatenate(frames)
+
+
+FILE = Path(__file__).parent / "sounddevice_audio_capture_demo.wav"
+sf.write(FILE, wave_data, samplerate=16000)
