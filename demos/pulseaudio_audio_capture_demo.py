25359f8f8c4d145142774c5da5e77c172e1665c0
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-10-27 17:31:11 +0900

    Add demo for PulseAudioApplicationAudioCapture

---
+++ demos/pulseaudio_audio_capture_demo.py
@@ -0,0 +1,37 @@
+"""This demo shows how to use SoundcardAudioCapture class."""
+from pathlib import Path
+
+import numpy as np
+import pulsectl
+import soundfile as sf
+
+from vrchat_io.audio import PulseAudioApplicationAudioCapture
+
+with pulsectl.Pulse("pulseaudio_application_audio_capture_demo") as pulse:
+    print("--- Available Target Applications ---")
+    client_names = []
+    for client in pulse.client_list():
+        print(client.name)
+        client_names.append(client.name)
+    print("-------------------------------------")
+    print()
+
+TARGET_APPLICATION = "Steam" if "Steam" in client_names else client_names[0]
+print("Selecting:", TARGET_APPLICATION)
+
+capture = PulseAudioApplicationAudioCapture(TARGET_APPLICATION, sample_rate=44100, buffer_size=4410)  # 0.1 sec
+
+frames = []
+for i in range(10):
+    frame = capture.read()
+    print(f"Frame: {i+1}, Shape: {frame.shape}")
+    print(f"Min: {abs(frame).min()}, Max: {abs(frame).max()}")
+    print()
+    frames.append(frame)
+
+wave_data = np.concatenate(frames)
+print("Wave data shape:", wave_data.shape)
+
+
+FILE = Path(__file__).parent / "pulseaudio_application_audio_capture_demo.wav"
+sf.write(FILE, wave_data, samplerate=44100)
